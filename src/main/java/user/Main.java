32849928d1f21773892d1b20050a5188958228a5
Kovács Nikolett <rue0103@gmail.com>
2020-04-09 11:51:18 +0200

    A Main és a UserDao elkészítése, a User class frissítése.

--- src/main/java/user/Main.java
+++ src/main/java/user/Main.java
@@ -1,4 +1,43 @@
 package user;
 
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test");
+        jdbi.installPlugin(new SqlObjectPlugin());
+        try (Handle handle = jdbi.open()) {
+            UserDao dao = handle.attach(UserDao.class);
+            User user = User.builder()
+                    .id(Long.valueOf(1))
+                    .username("Halp")
+                    .password("halpmeplease")
+                    .name("Halp halp")
+                    .email("halp@halp.com")
+                    .gender(User.Gender.FEMALE)
+                    .dob(LocalDate.parse("1920-11-11"))
+                    .enabled(true)
+                    .build();
+            dao.createTable();
+            dao.insert(user);
+            dao.findById(user.getId());
+            dao.findByUsername(user.getUsername());
+            System.out.println(user);
+            dao.delete(user);
+            dao.list().stream().forEach(System.out::println);
+        }
+
+    }
 }
+
+    //forma:
+    //User user = User.builder()
+    //        .name("James Bond")
+    //        .username("007")
+            // ...
+    //        .dob(LocalDate.parse("1920-11-11"))
+    //        .build();
--- src/main/java/user/UserDao.java
+++ src/main/java/user/UserDao.java
@@ -1,4 +1,46 @@
 package user;
 
-public class UserDao {
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
+import java.util.Optional;
+
+@RegisterBeanMapper(User.class)
+public interface UserDao {
+
+    @SqlUpdate("""
+        CREATE TABLE user (
+            id IDENTITY PRIMARY KEY,
+            username VARCHAR NOT NULL,
+            password VARCHAR NOT NULL,
+            name VARCHAR NOT NULL,
+            email VARCHAR,
+            gender ENUM('FEMALE','MALE') NOT NULL,
+            dob DATE,
+            enabled VARCHAR
+        )
+        """
+    )
+    void createTable();
+
+    @SqlUpdate("INSERT INTO user VALUES (:id, :username, :password, :name, :email, :gender, :dob, :enabled)")
+    @GetGeneratedKeys
+    Long insert(@BindBean User user);
+
+    @SqlQuery("SELECT * FROM user WHERE id = :id")
+    Optional<User> findById(@Bind("id") Long id);
+
+    @SqlQuery("SELECT * FROM user WHERE username = :username")
+    Optional<User> findByUsername(@Bind("username") String username);
+
+    @SqlUpdate("DELETE  FROM user WHERE username = :username")
+    void delete(@BindBean User user);
+
+    @SqlQuery("SELECT * FROM user ORDER BY id")
+    List<User> list();
 }
